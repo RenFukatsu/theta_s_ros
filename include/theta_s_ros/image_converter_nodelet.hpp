a402301806fc7bbb400190bb323a61eb2326be78
RenFukatsu <fukatsuren88@gmail.com>
2021-03-24 20:38:01 +0900

    print INFO -> DEBUG

--- include/theta_s_ros/image_converter_nodelet.hpp
+++ include/theta_s_ros/image_converter_nodelet.hpp
@@ -1,12 +1,13 @@
 #ifndef __IMAGE_CONVERTER_NODELET_HPP
 #define __IMAGE_CONVERTER_NODELET_HPP
 
-#include <ros/ros.h>
+#include <cv_bridge/cv_bridge.h>
 #include <nodelet/nodelet.h>
 #include <pluginlib/class_list_macros.h>
+#include <ros/ros.h>
 #include <sensor_msgs/Image.h>
+
 #include <opencv2/opencv.hpp>
-#include <cv_bridge/cv_bridge.h>
 
 #include "opencv_theta_s/EquirectangularConversion/ThetaConversion.hpp"
 
@@ -15,12 +16,13 @@ class ImageConverterNodelet : public nodelet::Nodelet {
  public:
     virtual void onInit();
     void image_callback(const sensor_msgs::ImageConstPtr&);
+
  private:
     ros::NodeHandle nh;
     ros::NodeHandle private_nh;
     ros::Subscriber image_sub;
     ros::Publisher image_pub;
 };
-} // namespace theta_s_ros
+}  // namespace theta_s_ros
 
-#endif // __IMAGE_CONVERTER_NODELET_HPP
+#endif  // __IMAGE_CONVERTER_NODELET_HPP
--- src/image_converter.cpp
+++ src/image_converter.cpp
@@ -1,4 +1,5 @@
 #include "theta_s_ros/image_converter.hpp"
+
 #include "opencv_theta_s/EquirectangularConversion/ThetaConversion.hpp"
 
 ImageConverter::ImageConverter() : private_nh("~") {
@@ -7,40 +8,40 @@ ImageConverter::ImageConverter() : private_nh("~") {
 }
 
 void ImageConverter::image_callback(const sensor_msgs::ImageConstPtr& received_image) {
-    ROS_INFO("start image_callback");
+    ROS_DEBUG("start image_callback");
     double start_time = ros::Time::now().toSec();
 
-    ROS_INFO("ros image to cv image");
+    ROS_DEBUG("ros image to cv image");
     cv_bridge::CvImageConstPtr cv_image_ptr;
     try {
         cv_image_ptr = cv_bridge::toCvCopy(received_image, sensor_msgs::image_encodings::BGR8);
-    } catch(cv_bridge::Exception& ex) {
+    } catch (cv_bridge::Exception& ex) {
         ROS_ERROR("cv_bridge exception: %s", ex.what());
         return;
     }
     cv::Mat cv_image(cv_image_ptr->image.rows, cv_image_ptr->image.cols, cv_image_ptr->image.type());
     cv_image = cv_image_ptr->image;
 
-    ROS_INFO("fisheye image to equirectangular image");
+    ROS_DEBUG("fisheye image to equirectangular image");
     ThetaConversion(cv_image.cols, cv_image.rows).doConversion(cv_image);
 
-    ROS_INFO("cv image to output ros image");
+    ROS_DEBUG("cv image to output ros image");
     sensor_msgs::ImagePtr output_image = cv_bridge::CvImage(std_msgs::Header(), "bgr8", cv_image).toImageMsg();
     output_image->header = received_image->header;
     image_pub.publish(output_image);
 
-    ROS_INFO("elapsed time : %f [sec]", ros::Time::now().toSec() - start_time);
+    ROS_INFO("[image_converter:image_callback] elapsed time : %f [sec]", ros::Time::now().toSec() - start_time);
 
     return;
 }
 
 void ImageConverter::process() {
-    ROS_INFO("start process");
+    ROS_DEBUG("start process");
     ros::spin();
     return;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     ros::init(argc, argv, "convert_ros_image_to_cv_image");
     ImageConverter image_convertor;
     image_convertor.process();
